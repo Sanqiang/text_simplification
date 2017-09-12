3fffa6a6bc4b63e6fa5688aae211dea3c50bfa47
Sanqiang <zhaosanqiang916@gmail.com>
2017-09-12 10:30:10 -0800

    fix tmp path

--- model/model_config.py
+++ model/model_config.py
@@ -1,8 +1,8 @@
 class DefaultConfig():
     batch_size = 25
     dimension = 512
-    max_complex_sentence = 200
-    max_simple_sentence = 200
+    max_complex_sentence = 100
+    max_simple_sentence = 100
 
     optimizer = 'adagrad'
     learning_rate = 0.001
@@ -14,5 +14,5 @@ class DefaultConfig():
     vocab_simple = '../data/dummy_simple_vocab'
     vocab_complex = '../data/dummy_complex_vocab'
 
-    logdir = '/Users/zhaosanqiang916/tmp'
-    outdir = '/Users/zhaosanqiang916/tmp'
+    logdir = '../../tmp'
+    outdir = '../../tmp'
