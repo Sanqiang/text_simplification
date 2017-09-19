6f34d451ae113e2a35859f3141203d58b2b6c1cf
memray <memray0@gmail.com>
2017-09-19 01:17:42 -0400

    fix non gpu session

--- util/session.py
+++ util/session.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 
 def get_session_config(model_config):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = model_config.allow_growth
+    config.gpu_options.per_process_gpu_memory_fraction = model_config.per_process_gpu_memory_fraction
     if not model_config.use_gpu:
         config = tf.ConfigProto(
             device_count={'GPU': 0}
         )
+        print('Not use GPU.')
     else:
         config = tf.ConfigProto()
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = model_config.allow_growth
-    config.gpu_options.per_process_gpu_memory_fraction = model_config.per_process_gpu_memory_fraction
+        print('Use GPU.')
     return config
