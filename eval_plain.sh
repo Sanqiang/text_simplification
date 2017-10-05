2fd062b2ec812c600689f2ec346e2037ad3894bc
Sanqiang <zhaosanqiang916@gmail.com>
2017-10-04 20:54:48 -0400

    add plain bash

--- eval_plain.sh
+++ eval_plain.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+#SBATCH --gres=gpu:1
+#SBATCH --cluster=gpu
+#SBATCH --partition=gtx1080
+#SBATCH --job-name=vl_pl
+#SBATCH --output=vl_pl.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+# Load modules
+module restore
+# module purge
+# module load cuda/8.0.44
+# module load tensorflow/1.3.0
+
+export PERL5LIB="/ihome/hdaqing/saz31/perl_lib"
+
+# Run the job
+srun python model/eval.py --framework transformer --output_folder plain --tied_embedding enc_dec --mode all
--- train_plain.sh
+++ train_plain.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+#SBATCH --cluster=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gtx1080
+#SBATCH --job-name=tr_pl
+#SBATCH --output=tr_pl.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+# Load modules
+module restore
+# module purge
+# module load cuda/8.0.44
+# module load tensorflow/1.3.0
+
+# Run the job
+srun python model/train.py --framework transformer --output_folder plain --tied_embedding enc_dec
