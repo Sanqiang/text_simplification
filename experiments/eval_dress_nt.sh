ff1b2c5794afc1cc925e040de0340edd6493b05a
Sanqiang <zhaosanqiang916@gmail.com>
2017-10-11 12:07:31 -0400

    add no pe experiment

---
+++ experiments/eval_dress_nt.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --gres=gpu:1
+#SBATCH --cluster=gpu
+#SBATCH --partition=gtx1080
+#SBATCH --job-name=vdr_nt
+#SBATCH --output=vdr_nt.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+# Load modules
+module restore
+
+export PERL5LIB="/ihome/hdaqing/saz31/perl_lib"
+
+# Run the job
+srun python ../model/eval.py -fw transformer -out dr_nt --mode all -pos none
