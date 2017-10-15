d42347e8afd1cd6f26001948874c25b0f6e4c5ee
Sanqiang <zhaosanqiang916@gmail.com>
2017-10-15 01:01:12 -0400

    finalize seq2seq

--- experiments/train_dress_s2s.sh
+++ experiments/train_dress_s2s.sh
@@ -2,8 +2,8 @@
 #SBATCH --cluster=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gtx1080
-#SBATCH --job-name=tdr
-#SBATCH --output=tdr.out
+#SBATCH --job-name=tdr_s2s
+#SBATCH --output=tdr_s2s.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -12,4 +12,4 @@
 module restore
 
 # Run the job
-srun python ../model/train.py -fw transformer -out dr -layer_drop 0.2 -op adam -lr 0.001 --mode dress
+srun python ../model/train.py -fw seq2seq -out dr_s2s -layer_drop 0.2 -op adam -lr 0.001 --mode dress
