0ce8d5c004d4fc014bd7ebfeb88da781b1dfe8d9
Sanqiang <zhaosanqiang916@gmail.com>
2017-10-20 13:21:32 -0400

    change h256 exp for 2 layer

--- experiments/train_dress_h256.sh
+++ experiments/train_dress_h256.sh
@@ -12,4 +12,4 @@
 module restore
 
 # Run the job
-srun python ../model/train.py -fw transformer -out dr_h256 -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -dim 256 -nh 4
+srun python ../model/train.py -fw transformer -out dr_h256 -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -dim 256 -nhl 2 -nel 2 -ndl 2
