a37d657ca448525831422beb41fd1caace8f49a2
Sanqiang <zhaosanqiang916@gmail.com>
2017-10-23 18:47:40 -0400

    add codelab for fkgl

--- codelab/fkgl_gt.py
+++ codelab/fkgl_gt.py
@@ -3,12 +3,12 @@ from util.fkgl import Readability
 
 
 # path = '/Users/zhaosanqiang916/git/text_simplification_data/test/test.8turkers.tok.norm'
-path = '/Users/zhaosanqiang916/git/text_simplification_data/train/dress/wikilarge/wiki.full.aner.train.src'
+path = '/Users/zhaosanqiang916/git/text_simplification_data/train/dress/wikilarge/wiki.full.aner.train.dst'
 text = open(path, encoding='utf-8').read()
 rd = Readability(text)
 fkgl = rd.FleschKincaidGradeLevel()
 print(fkgl)
 
 # Report:
-# For 8 ref dataset : 9.5088 -> 8.0282
-# For Dress train:
+# For 8 ref dataset: 9.5088 -> 8.0282
+# For Dress train: 11.3091 -> 8.2863
