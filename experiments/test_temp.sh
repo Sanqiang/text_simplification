a0121999619e0c827ec8948761df54199f13c1ad
Sanqiang <zhaosanqiang916@gmail.com>
2017-11-17 02:32:56 -0500

    hot fix for test_temp.sh

--- experiments/test_temp.sh
+++ experiments/test_temp.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --cluster=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --partition=titanx
-#SBATCH --job-name=tdr_l4_ppdb_args0
-#SBATCH --output=tdr_l4_ppdb_args0.out
+#SBATCH --partition=gtx1080
+#SBATCH --job-name=tdr_l4_ppdb_args
+#SBATCH --output=tdr_l4_ppdb_args.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -12,4 +12,4 @@
 module restore
 
 # Run the job
-srun python ../model/test.py -fw transformer -out dr_l4_ppdb_args0 -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -nhl 4 -nel 4 -ndl 4 -ppdb 'comp' -lc True -ppdbw 2.0|1.0 --test_ckpt /ihome/hdaqing/saz31/sanqiang/dr_l4_ppdb_args/modelmodel.ckpt-749572
+srun python ../model/test.py -fw transformer -out dr_l4_ppdb_args -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -nhl 4 -nel 4 -ndl 4 -ppdb 'comp' -lc True -ppdbw 2.0|1.5 --test_ckpt /ihome/hdaqing/saz31/sanqiang/dr_l4_ppdb_args/model/model.ckpt-749572
