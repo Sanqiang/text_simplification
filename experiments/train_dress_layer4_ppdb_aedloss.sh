0a54299756ff5ade18cb2b074de6b932b9075d70
Sanqiang <zhaosanqiang916@gmail.com>
2017-12-02 16:17:20 -0500

    add aedloss exp

---
+++ experiments/train_dress_layer4_ppdb_aedloss.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH --cluster=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gtx1080
+#SBATCH --job-name=tdr_l4_ppdb_aedloss
+#SBATCH --output=tdr_l4_ppdb_aedloss.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+# Load modules
+module restore
+
+# Run the job
+srun python ../model/train.py -fw transformer -out dr_l4_ppdb_aedloss -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -nhl 4 -nel 4 -ndl 4 -ppdb 'comp' -lc True -ppdbw 2.0|1.5|1.0 --attn_loss enc_dec@1
---
+++ experiments/train_dress_layer4_ppdb_aedloss0.3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH --cluster=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gtx1080
+#SBATCH --job-name=tdr_l4_ppdb_aedloss0.3
+#SBATCH --output=tdr_l4_ppdb_aedloss0.3.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+# Load modules
+module restore
+
+# Run the job
+srun python ../model/train.py -fw transformer -out dr_l4_ppdb_aedloss0.3 -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -nhl 4 -nel 4 -ndl 4 -ppdb 'comp' -lc True -ppdbw 2.0|1.5|0.3 --attn_loss enc_dec@0.3
