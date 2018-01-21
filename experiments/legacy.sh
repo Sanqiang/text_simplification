82f24943f10a0e1fa1208572a3eccb92cd289581
Sanqiang <zhaosanqiang916@gmail.com>
2018-01-21 18:05:44 -0500

    add experiment for legacy memory

--- experiments/legacy.sh
+++ experiments/legacy.sh
@@ -2,8 +2,8 @@
 #SBATCH --cluster=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --partition=titanx
-#SBATCH --job-name=trans_base_legacy
-#SBATCH --output=trans_base_legacy.out
+#SBATCH --job-name=legacy
+#SBATCH --output=legacy.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -14,6 +14,7 @@
 module restore
 
 # Run the job
-srun python ../model/train.py -ngpus 2 -bsize 89 -fw transformer -out trans_base_legacy -layer_drop 0.2 -op adagrad -lr 0.1 --mode wikilegacy -nhl 4 -nel 4 -ndl 4 -lc True
+srun python ../model/train.py -ngpus 2 -bsize 64 -fw transformer -out legacy -layer_drop 0.2 -op adagrad -lr 0.1 --mode wikilegacy -nhl 4 -nel 4 -ndl 4 -lc True
+
 
 
--- experiments/legacy_memory.sh
+++ experiments/legacy_memory.sh
@@ -2,18 +2,19 @@
 #SBATCH --cluster=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --partition=titanx
-#SBATCH --job-name=trans_base_legacy
-#SBATCH --output=trans_base_legacy.out
+#SBATCH --job-name=legacy_mem
+#SBATCH --output=legacy_mem.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=6-00:00:00 # This is the max
 #SBATCH --qos=long
+#SBATCH --mem=16g
 
 # Load modules
 module restore
 
 # Run the job
-srun python ../model/train.py -ngpus 2 -bsize 89 -fw transformer -out trans_base_legacy -layer_drop 0.2 -op adagrad -lr 0.1 --mode wikilegacy -nhl 4 -nel 4 -ndl 4 -lc True
+srun python ../model/train.py -ngpus 2 -bsize 64 -fw transformer -out legacy_mem -layer_drop 0.2 -op adagrad -lr 0.1 --mode wikilegacy -nhl 4 -nel 4 -ndl 4 -lc True --memory rule --memory_prepare_step 50000
 
 
