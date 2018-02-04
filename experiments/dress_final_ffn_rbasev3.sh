6e9c918455a32693b218886303caafd99947064f
Sanqiang <zhaosanqiang916@gmail.com>
2018-02-04 04:59:35 -0500

    add new bash for control qos

--- experiments/dress_final_ffn_rbasev3.sh
+++ experiments/dress_final_ffn_rbasev3.sh
@@ -1,19 +1,51 @@
 #!/usr/bin/env bash
+partition=$1
+qos=$2
+name=$3
+
+if [[ -z $3 ]]; then
+    name='dress_final_ffn_rbv3_op'
+fi
+
+if [ $1 = "g" ]; then
+    partition='gtx1080'
+elif [ $1 = "tx" ]; then
+    partition='titanx'
+elif [ $1 = "t" ]; then
+    partition='titan'
+fi
+
+
+if [ $2 = "s" ]; then
+    time='1-00:00:00'
+    qos='short'
+elif [ $2 = "n" ]; then
+    time='3-00:00:00'
+    qos='normal'
+elif [ $2 = "l" ]; then
+    time='6-00:00:00'
+    qos='long'
+fi
+
 #SBATCH --cluster=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gtx1080
-#SBATCH --job-name=dress_final_ffn_sing
-#SBATCH --output=dress_final_ffn_sing.out
+#SBATCH --partition=$partition
+#SBATCH --job-name=$name
+#SBATCH --output=$name'.out'
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=6-00:00:00 # This is the max
-#SBATCH --qos=long
+#SBATCH --time=$time
+#SBATCH --qos=$qos
+
+echo 'Task:' $name
+echo 'Use Parititon:' $partition
+echo 'WIth Time Length:' $time
 
 # Load modules
 module restore
 
 # Run the job
-srun python ../model/train.py -ngpus 1 -bsize 64 -fw transformer -out dress_final_ffn_sing -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -nhl 4 -nel 4 -ndl 4 -lc True --memory rule --memory_prepare_step 50000 -memcfg cffn_mofinal -rbase sing
+srun python ../model/train.py -ngpus 1 -bsize 64 -fw transformer -out $name -layer_drop 0.2 -op adagrad -lr 0.1 --mode dress -nhl 4 -nel 4 -ndl 4 -lc True --memory rule --memory_prepare_step 50000 -memcfg cffn_mofinal -rbase v3 -warm /zfs1/hdaqing/saz31/text_simplification/dress_final_ffn/model/model.ckpt-789741
 
 
